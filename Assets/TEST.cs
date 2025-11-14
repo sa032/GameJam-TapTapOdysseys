990717dd7a8819335d19da698b721c2bae07a16f
sa032 <satang0035@gmail.com>
2025-11-14 13:08:35 +0700

    Setup

---
+++ Assets/TEST.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    public int XD;
+    void Start()
+    {
+        print("HAHAHA");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
