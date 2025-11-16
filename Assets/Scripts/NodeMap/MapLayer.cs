d23f432635b43ac220c57bad806361a8b0cb09e4
sa032 <satang0035@gmail.com>
2025-11-16 12:37:07 +0700

    Fix bug #1

--- Assets/Scripts/NodeMap/MapLayer.cs
+++ Assets/Scripts/NodeMap/MapLayer.cs
@@ -8,7 +8,9 @@ namespace Map
         public NodeType nodeType;
         [Tooltip("If Random = 0. 100% chance to get this node")]
         [Range(0f, 1f)] public float randomizeNodes;
+        [Tooltip("If this is set to 0, nodes on this layer will appear in a straight line. Closer to 1f = more position randomization")]
         public float nodesApartDistance;
+        
         public float distanceFromPreviousLayer;
     }
 }
