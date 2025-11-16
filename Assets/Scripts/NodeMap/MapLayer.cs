c4e58a1376d8728bceec12c1fbbf31a329ada4c0
sa032 <satang0035@gmail.com>
2025-11-16 11:47:00 +0700

    Setup script nodemap

--- Assets/Scripts/NodeMap/MapLayer.cs
+++ Assets/Scripts/NodeMap/MapLayer.cs
@@ -8,5 +8,7 @@ namespace Map
         public NodeType nodeType;
         [Tooltip("If Random = 0. 100% chance to get this node")]
         [Range(0f, 1f)] public float randomizeNodes;
+        public float nodesApartDistance;
+        public float distanceFromPreviousLayer;
     }
 }
