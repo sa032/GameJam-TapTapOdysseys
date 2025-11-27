38e38e271ff05d6a0ffcca98ca069d0aaa4e496a
BoisMort <jthanakrit1@hotmail.com>
2025-11-27 21:21:38 +0700

    Added Post-Processing

--- Assets/Scripts/DamageText.cs
+++ Assets/Scripts/DamageText.cs
@@ -8,12 +8,12 @@ public class DamageText : MonoBehaviour
     public float damageNumber;
     private void Start()
     {
-        transform.localPosition = transform.localPosition + (Vector3)Random.insideUnitCircle * 1f;
+        transform.localPosition = transform.localPosition + (Vector3)Random.insideUnitCircle * 0.35f;
         launch = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
         tmp = GetComponent<TextMeshPro>();
         tmp.text = damageNumber.ToString();
-        float ClampX = Mathf.Clamp(0.1f*damageNumber,3,15);
-        float ClampY = Mathf.Clamp(0.1f*damageNumber,3,15);
+        float ClampX = Mathf.Clamp(0.05f*damageNumber,3,15);
+        float ClampY = Mathf.Clamp(0.05f*damageNumber,3,15);
         transform.localScale = new Vector3(ClampX, ClampY, 1);
 
         Destroy(gameObject, 5);
@@ -26,6 +26,6 @@ public class DamageText : MonoBehaviour
             6 * Time.deltaTime
         );
 
-        transform.localPosition += launch * 6 * Time.deltaTime;
+        transform.localPosition += launch * 3 * Time.deltaTime;
     }
 }
