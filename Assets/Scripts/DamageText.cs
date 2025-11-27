791fde29f2d285235de669d085f333793cd6e63a
BoisMort <jthanakrit1@hotmail.com>
2025-11-27 10:30:26 +0700

    hh

--- Assets/Scripts/DamageText.cs
+++ Assets/Scripts/DamageText.cs
@@ -4,4 +4,19 @@ using UnityEngine;
 public class DamageText : MonoBehaviour
 {
     private TextMeshPro tmp;
+    public float damageNumber;
+    public float lerpSpeed;
+    private void Start()
+    {
+        transform.position = Random.insideUnitCircle * 0.2f;
+    }
+    private void Update()
+    {
+        transform.localScale -= new Vector3(0.1f, 0.1f, 0);
+        transform.position = Vector3.Lerp(
+            transform.position,
+            transform.position + Vector3.up,
+            Time.deltaTime * lerpSpeed
+        );
+    }
 }
